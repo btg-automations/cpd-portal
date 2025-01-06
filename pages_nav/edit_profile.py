e75264e1448394358968d254e7f5a8f82c4001cb
chaseydev <chaseysison.dev@gmail.com>
2025-01-06 15:03:26 +0800

    fix: saving yearly hours goal

--- pages_nav/edit_profile.py
+++ pages_nav/edit_profile.py
@@ -11,7 +11,6 @@ def edit_profile():
     user_info = [record for record in user_data if record.get('email', '') == st.session_state.user_profile['mail']][0]
 
     with st.form(key='edit_form'):
-        # full_name = st.text_input("Name", user_info.get('full_name', ''))
         yearly_hours_goal = st.number_input("Yearly CPD Hours Goal", value=user_info.get('yearly_hours_goal', 0.0), min_value=40, max_value=5000, step=1)
 
         # Submit
@@ -19,21 +18,7 @@ def edit_profile():
 
         if submit_button:
             index = user_data.index(user_info)
-            user_data[index] = {
-                "email": user_info.get('email', ''),
-                "password": user_info.get('password', ''),
-                "yearly_hours_goal": yearly_hours_goal,
-                "user_type": user_info.get('user_type', '')
-            }
+            user_data[index]["yearly_hours_goal"] = yearly_hours_goal
+            print(user_data[index]["yearly_hours_goal"])
             save_data(user_data, users_file)
             st.success("User information updated successfully!")
-
-    # Finalize authentication handling first
-    # with st.form(key='change_password_form'):
-    #     st.write("Change Password")
-    #     old_password = st.text_input("Old Password", type="password")
-    #     new_password = st.text_input("New Password", type="password")
-    #     new_password_confirm = st.text_input("Confirm New Password", type="password")
-    #     if st.form_submit_button("Submit"):
-    #         # handle password change
-    #         st.success("Password changed successfully!")
